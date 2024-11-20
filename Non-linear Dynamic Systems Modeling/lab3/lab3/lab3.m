d2dcc99a44b73077620a82729573719c2fcdef13
Gregory-Hub <novikov.gri@gmail.com>
2024-11-20 14:17:02 +0300

    lab3

--- Non-linear Dynamic Systems Modeling/lab3/lab3/lab3.m
+++ Non-linear Dynamic Systems Modeling/lab3/lab3/lab3.m
@@ -4,24 +4,30 @@ a = 0.2;
 b = 0.2;
 c = 5.7;
 
-eps = 1e-12;
 x = [0 0 0];
-h = 1e-3;
+eps = 1e-8;
+h = 1e-2;
 max_time = 1000;
-interval_time = 50;
+interval_time = 0.5;
 solve = @solve_cd;
 
-x1 = x + eps;
-n = interval_time / h;
+p_step = 1e-4;
+min_p = p_step;
+max_p = 0.1;
 
-lle = 0;
-for i = 1 : max_time / interval_time
-    X = solve(x, n, h, a, b, c);
-    X1 = solve(x1, n, h, a, b, c);
-    x = X(end, :);
-    x1 = X1(end, :);
-    lle = lle + log10(x1 / x);
-    x1 = x + eps * (x1 - x) / (norm(x1 - x));
+p = min_p : p_step : max_p;
+
+figure
+xlabel('h')
+ylabel('LLE')
+hold on
+
+LLE = zeros(size(p));
+
+for i = 1 : length(p)
+    lle = find_lle(p(i), x, max_time, interval_time, eps, solve, a, b, c);
+    LLE(i) = lle;
 end
 
-lle = lle / max_time
+loglog(p, LLE)
+grid on
