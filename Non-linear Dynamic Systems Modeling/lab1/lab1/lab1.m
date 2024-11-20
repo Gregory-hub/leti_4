cb4b7d4108ca444d81106700d8cb5ce2526b3be8
Gregory-Hub <novikov.gri@gmail.com>
2024-11-20 14:16:28 +0300

    lab1

--- Non-linear Dynamic Systems Modeling/lab1/lab1/lab1.m
+++ Non-linear Dynamic Systems Modeling/lab1/lab1/lab1.m
@@ -2,14 +2,14 @@ clear
 
 X0 = [0 0 0];
 h = 1e-3;
-n = 1e3;
+n = 1e6;
 
-X = solve_euler(X0, n, h);
+% X = solve_euler(X0, n, h);
 % X = solve_midpoint(X0, n, h);
 % X = solve_cd(X0, n, h, 0.5);
-% X = solve_dopri8(X0, n, h);
+X = solve_dopri8(X0, n, h);
 
 % plot_3d_time(h, X);
-% plot_3d_phase(X);
+plot_3d_phase(X);
 
-compare_to_dopri(X, X0, n, h);
+% compare_to_dopri(X, X0, n, h);
