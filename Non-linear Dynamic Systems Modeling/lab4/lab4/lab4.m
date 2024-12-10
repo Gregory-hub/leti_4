4fa47aaee081d1dcf5c73b0ca444553c462cfe48
Gregory-Hub <novikov.gri@gmail.com>
2024-12-10 16:08:32 +0300

    lab4 not finished

--- Non-linear Dynamic Systems Modeling/lab4/lab4/lab4.m
+++ Non-linear Dynamic Systems Modeling/lab4/lab4/lab4.m
@@ -5,28 +5,28 @@ b = 0.2;
 c = 5.7;
 
 x = [0 0 0];
-h = 1e-3;
-max_time = 1000;
-solve = @solve_euler;
+h = 1e-2;
+max_time = 2000;
+solve = @solve_cd;
 
 n = max_time / h;
 
-p_step = 1e-2;
-min_p = 0;
-max_p = 2;
+p_step = 20e-3;
+min_p = 1;
+max_p = 10;
 
 p = min_p : p_step : max_p;
 
 figure
-xlabel('b')
+xlabel('c')
 ylabel('SE')
 hold on
 
 SE = zeros(size(p));
 
 for i = 1 : length(p)
-    X = solve(x, n, h, a, p(i), c);
-    se = find_spectral_entropy(X(:, 1));
+    X = solve(x, n, h, a, b, p(i));
+    se = find_spectral_entropy(X(floor(9 * length(X) / 10) : end, 1));
     SE(i) = se;
 end
 
